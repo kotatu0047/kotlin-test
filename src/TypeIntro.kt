66d76c68cab627e7c0a7eb95f3f09cdaed93f8c6
kotatu <yamamichi1995@outlook.jp>
2025-02-13 16:55:49 +0900

    section 3

--- src/TypeIntro.kt
+++ src/TypeIntro.kt
@@ -1,7 +1,7 @@
 const val  MAX_EXPERIENCE: Int = 5000
 
 fun main(args: Array<String>) {
-    val playerName = "Estragon"
+    val playerName = "Tetragon"
 //    playerName = "Vladimir" // Error: Val cannot be reassigned
     var experiencePoints = 5
     experiencePoints += 5  // OK
