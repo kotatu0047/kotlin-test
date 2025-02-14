ad8e71d12d215e5c47c9e9b4a05800d8bdc95ee6
kotatu <yamamichi1995@outlook.jp>
2025-02-14 09:39:50 +0900

    section 7

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,7 +1,39 @@
-import java.util.*
+const val TAVEN_NAME = "Taernyl's Folly"
 
 fun main(args: Array<String>) {
-    var beverage = readlnOrNull() ?: "Buttered Ale"
+//    placeOrder("shandy,Dragon's Breath,5.91")
+    placeOrder("elixir,Shirley's Temple,4.12")
 
-    println(beverage)
+    val phrase = "Ah, delicious $TAVEN_NAME!"
+    println("Madrigal exclaims: ${toDragonSpeak(phrase)}")
 }
+
+private fun placeOrder(menuData: String) {
+    val indexOfApostrophe = TAVEN_NAME.indexOf('\'')
+    val tavernMaster = TAVEN_NAME.substring(0..<indexOfApostrophe)
+    println("Madrigal speaks with $tavernMaster about their order.")
+
+    val (type, name, price) = menuData.split(',')
+
+    val message = "Madrigal buys a $name ($type) for $price."
+    println(message)
+
+    val phrase = if (name == "Dragon's Breath") {
+        "Madrigal exclaims: ${toDragonSpeak("Ah, delicious $name!")}"
+    } else {
+        "Madrigal says: Thanks for the $name."
+    }
+    println(phrase)
+}
+
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiou]")) {
+        when (it.value) {
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
+    }
